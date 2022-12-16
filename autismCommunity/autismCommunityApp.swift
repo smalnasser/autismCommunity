ad07f42b7d02fec3f6a4b8da9ec05293ad9b5ee6
Gyda <90142160+gyda13@users.noreply.github.com>
2022-12-16 19:43:22 +0300

    Initial Commit

---
+++ autismCommunity/autismCommunityApp.swift
@@ -0,0 +1,17 @@
+//
+//  autismCommunityApp.swift
+//  autismCommunity
+//
+//  Created by gyda almohaimeed on 22/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct autismCommunityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
